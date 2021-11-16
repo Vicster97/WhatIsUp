180f80b5d92a0bc1b4180141f0437538758728ae
Veronika Ulanova <veronikaulanova@Veronikas-Air.wind3.hub>
2021-11-16 11:51:57 +0100

    adding extra info to site

--- whatisup_frontend/src/Pages/Home.js
+++ whatisup_frontend/src/Pages/Home.js
@@ -8,6 +8,7 @@ function Home() {
             <Navbar/>
             <div className="page-body">
                 <h1 className="page-welcome-message">Welcome to Home page of WhatIsUp?</h1>
+                <h3 className="page-message"> This website is build with the purpose of analysing, visualising and improve the undertsanding of the climate processes in the atmosphere. </h3>
             </div>
         </div>
     );
