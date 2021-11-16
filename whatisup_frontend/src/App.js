505acd839b8724cf6b91709ea053953d52945389
Veronika Ulanova <veronikaulanova@Veronikas-Air.wind3.hub>
2021-11-16 17:15:09 +0100

    fixing react-router-dom

--- whatisup_frontend/src/App.js
+++ whatisup_frontend/src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home';
 import Data from './Pages/Data';
 import Hello from './Pages/Hello';
 
 function App() {
   return (
-    <Router basename="/">
-      <Route exact path='' component={Hello}/>
-      <Route exact path="/home" component={Home}/>
-      <Route exact path='/data' component={Data}/>
-    </Router>
+    <Routes>
+      <Route path='/*' element={<Hello/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path='/data' element={<Data/>}/>
+    </Routes>
   );
 }
 
